900f11d0951b52b8442f7abcb01d516d744dcd06
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-23 11:04:40 -0500

    pizza.html added

--- pizzas/views.py
+++ pizzas/views.py
@@ -12,3 +12,11 @@ def pizzas(request):
     pizzas = Pizza.objects.order_by("name")
     context = {"pizzas": pizzas}
     return render(request, "pizzas/pizzas.html", context)
+
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by("name")
+    context = {"pizza": pizza, "toppings": toppings}
+
+    return render(request, "pizzas/pizza.html", context)
