16e21e148ca312db85d9a6391ea8145a64f598b0
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-23 10:53:22 -0500

    Added pizza.html

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "pizzas"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("pizzas", views.pizzas, name="pizzas"),
+]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "pizzas/index.html")
+
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by("name")
+    context = {"pizzas": pizzas}
+    return render(request, "pizzas/pizzas.html", context)
