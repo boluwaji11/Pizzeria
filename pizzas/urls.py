c5c59048aafb0feb4b2de110002df95128076649
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-24 22:08:51 -0500

    minor edits

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("pizzas", views.pizzas, name="pizzas"),
     path("pizzas/<int:pizza_id>", views.pizza, name="pizza"),
+    # path("comments/<int:pizza_id>", views.comment, name="comment"),
+    path("comment/<int:pizza_id>", views.comment, name="comment"),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Pizza
+from django.shortcuts import render, redirect
+from .models import Pizza, Topping, Comment
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -17,6 +18,27 @@ def pizzas(request):
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.topping_set.order_by("name")
-    context = {"pizza": pizza, "toppings": toppings}
+    comments = pizza.comment_set.order_by("name")
 
+    context = {"pizza": pizza, "toppings": toppings, "comments": comments}
     return render(request, "pizzas/pizza.html", context)
+
+
+def comment(request, pizza_id):
+    comment = Pizza.objects.get(id=pizza_id)
+    comments = comment.comment_set.order_by("name")
+
+    if request.method != "POST":
+        form = CommentForm()
+    else:
+        form = CommentForm(data=request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.comment = comment
+            new_comment.save()
+            form.save()
+            return redirect("pizzas:pizza", pizza_id=pizza_id)
+
+    context = {"form": form, "comment": comment}
+    return render(request, "pizzas/comment.html", context)
