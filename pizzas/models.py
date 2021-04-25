535c2f4fcbf003bc27f7df703050d9b1fa52bbb2
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-25 00:50:05 -0500

    comment form added

--- pizzas/models.py
+++ pizzas/models.py
@@ -22,7 +22,7 @@ class Comment(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    body = models.TextField()
+    comment = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Pizza, Topping
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -18,5 +20,32 @@ def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.topping_set.order_by("name")
 
-    context = {"pizza": pizza, "toppings": toppings}
+    # List of active comments for this pizza
+    comments = pizza.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method != "POST":
+        comment_form = CommentForm()
+    else:
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            # Create Comment object but doesn't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current pizza to the comment
+            new_comment.pizza = pizza
+            # Save the comment to the database
+            new_comment.save()
+            messages.success(request, "Your comment has been added successfully!")
+            return redirect("pizzas:pizza", pizza_id=pizza_id)
+
+    context = {
+        "pizza": pizza,
+        "toppings": toppings,
+        "comments": comments,
+        "new_comment": new_comment,
+        "comment_form": comment_form,
+    }
+
     return render(request, "pizzas/pizza.html", context)
