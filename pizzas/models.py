8ac109834eadc51f9cfc89f0e2a400ddcbcb9277
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-24 22:24:59 -0500

    comment model created

--- pizzas/models.py
+++ pizzas/models.py
@@ -16,3 +16,19 @@ class Topping(models.Model):
 
     def __str__(self):
         return f"{self.name[:50]}..."
+
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self):
+        return "Comment by {} on {}".format(self.name, self.pizza)
